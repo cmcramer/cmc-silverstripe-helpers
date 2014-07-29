8765206341f1d3de3a82d808bc73a5e6b0961554
Cathy Cramer <cmc@orionhost.net>
2014-07-28 20:34:46 -0600

    Added CmcDateHelper

---
+++ code/helper/CmcDateHelper.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * 
+ * @author cmc
+ * 
+ * @todo add translate if DateTime doesn't translate automatically based on location
+ *
+ */
+class CmcDateHelper {
+	
+	private $cocdate; //DateTime
+	private $dateWeek; //String
+	private $weekStartDate 	= null; //DateTime
+	private $weekEndDate	= null; //DateTime
+	private $sameWeekMonths	= null; //Boolean
+	private $sameWeekYears	= null;
+	
+	private static $contMark = ' - ';
+	
+	public function __construct($strDate='') {
+		$this->cocdate = new DateTime($strDate); //will set date to now if no date string
+	}
+	
+	public function YearLabel() {
+		return $this->cocdate->format('Y');
+	}
+	
+	public function MonthYearLabel() {
+		return $this->cocdate->format('F Y');
+	}
+	
+	public function ShortMonthYearLabel() {
+		return $this->cocdate->format('M Y');
+	}
+	
+
+	public function DayMonthYearLabel() {
+		return $this->cocdate->format('j F Y');
+	}
+	
+	public function DayShortMonthYearLabel() {
+		return $this->cocdate->format('j M Y');
+	}
+	
+	public function WeekLabel() {
+		//Year and Month Same
+		if ($this->isSameWeekYears() && $this->isSameWeekMonths()) {
+			return $this->getWeekStartDate()->format('j') .self::$contMark.
+					$this->getWeekEndDate()->format('j F Y');
+		//Only Year same
+		} elseif ($this->isSameWeekYears()) { 
+			return $this->getWeekStartDate()->format('j F') .self::$contMark.
+					$this->getWeekEndDate()->format('j F Y');
+		}
+		
+		//Month and Year different
+		return $this->getWeekStartDate()->format('j F Y') .self::$contMark.
+				$this->getWeekEndDate()->format('j F Y');
+		
+	}	
+	
+	public function WeekShortMonthLabel() {
+		//Year and Month Same
+		if ($this->isSameWeekYears() && $this->isSameWeekMonths()) {
+			return $this->getWeekStartDate()->format('j') .self::$contMark.
+					$this->getWeekEndDate()->format('j M Y');
+		//Only Year same
+		} elseif ($this->isSameWeekYears()) { 
+			return $this->getWeekStartDate()->format('j M') .self::$contMark.
+					$this->getWeekEndDate()->format('j M Y');
+		}
+		
+		//Month and Year different
+		return $this->getWeekStartDate()->format('j M Y') .self::$contMark.
+				$this->getWeekEndDate()->format('j M Y');
+		
+	}
+	
+	//private functions used to determine Week string
+	//more lazy instantiation
+	private function isSameWeekMonths() {
+		//don't have to check year because range only spans one week.
+		if ( $this->sameWeekMonths === null ) {
+			if ($this->getWeekStartDate()->format('M') == $this->getWeekEndDate()->format('M')) {
+				$this->sameWeekMonths = true;
+			} else {
+				$this->sameWeekMonths = false;
+			}
+		}
+		return $this->sameWeekMonths;
+	}
+
+	private function isSameWeekYears() {
+		//don't have to check year because range only spans one week.
+		if ( $this->sameWeekYears === null ) {
+			if ($this->getWeekStartDate()->format('Y') == $this->getWeekEndDate()->format('Y')) {
+				$this->sameWeekYears = true;
+			} else {
+				$this->sameWeekYears = false;
+			}
+		}
+		return $this->sameWeekYears;
+	}
+	
+	
+
+	//week, start and end of week lazily instantiated
+	private function getDateWeek() {
+		if ( $this->dateWeek === null ) {
+			$this->dateWeek = $this->cocdate->format("w");
+			//Debug::show($this->dateWeek);
+		}
+		return $this->dateWeek;
+	}
+	
+	private function getWeekStartDate() {
+		if ( $this->weekStartDate === null ) {
+			$numDateWeek = $this->getDateWeek();
+			$this->weekStartDate = clone $this->cocdate;
+			$this->weekStartDate->modify("-".$this->getDateWeek()." days");
+			//Debug::show($this->weekStartDate);
+		}
+		return $this->weekStartDate;
+	}
+	
+	private function getWeekEndDate() {
+		if ( $this->weekEndDate === null ) {
+			$this->weekEndDate = clone $this->getWeekStartDate();
+			$this->weekEndDate->modify("+6 days");
+			//Debug::show($this->weekEndDate);
+		}
+		return $this->weekEndDate;
+	}
+	
+	
+		
+
+	
+}
