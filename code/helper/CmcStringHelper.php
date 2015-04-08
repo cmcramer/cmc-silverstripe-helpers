5c58b13f66f432b0ca3ae588e727e58109dede69
Cathy Cramer <cmc@orionhost.net>
2015-04-08 12:17:50 -0600

    Don't allow spaces

--- code/helper/CmcStringHelper.php
+++ code/helper/CmcStringHelper.php
@@ -5,7 +5,7 @@
 class CmcStringHelper {
     
     public static function alphanumericWithDashes($str) {
-        $strSafe = preg_replace("/[^A-Za-z0-9 ]/", '-', $str);
+        $strSafe = preg_replace("/[^A-Za-z0-9]/", '-', $str);
         return $strSafe;
     }
 }
