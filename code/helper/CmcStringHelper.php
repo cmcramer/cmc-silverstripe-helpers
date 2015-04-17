006bf90b04307a7027a8fb282969ec2c11a9a723
Cathy Cramer <cmc@orionhost.net>
2015-04-17 11:47:32 -0600

    Add alphaNumericWithSpaces()
    Allows spaces, but other non-alphanumeric characters are stripped.

--- code/helper/CmcStringHelper.php
+++ code/helper/CmcStringHelper.php
@@ -4,8 +4,19 @@
  */
 class CmcStringHelper {
     
+    /*
+     * @description spaces and other non-alphanumeric characters are replaced with dashes
+     */
     public static function alphanumericWithDashes($str) {
         $strSafe = preg_replace("/[^A-Za-z0-9]/", '-', $str);
+        return $strSafe;
+    }
+    
+    /*
+     * @description Allows spaces, but other non-alphanumeric characters are stripped.
+     */
+    public static function alphanumericWithSpaces($str) {
+        $strSafe = preg_replace("/[^A-Za-z0-9 ]/", '', $str);
         return $strSafe;
     }
 }
