4ef836da1b2f7bb8ab578807b3c50c83ccf304b8
Cathy Cramer <cmc@orionhost.net>
2015-08-28 11:49:03 -0600

    Add Logo2 and LogoUrls

--- code/dataextensions/CmcSiteLogo.php
+++ code/dataextensions/CmcSiteLogo.php
@@ -19,9 +19,18 @@
 class CmcSiteLogo extends DataExtension {
     public static $has_one = array(
         'LogoImage' => 'Image',
+        'Logo2Image' => 'Image',
+    );
+    
+    public static $db = array(
+        'LogoUrl'   => 'Varchar(250)',
+        'Logo2Url'   => 'Varchar(250)',
     );
     
     public function updateCMSFields(FieldList $fields) {
         $fields->addFieldsToTab('Root.Main', new UploadField('LogoImage', 'Choose an image for your site logo'));
+        $fields->addFieldToTab("Root.Main", new TextField("LogoUrl", "Optionally enter URL to link to from logo image"));
+        $fields->addFieldsToTab('Root.Main', new UploadField('Logo2Image', 'Choose an image for your 2nd site logo'));
+        $fields->addFieldToTab("Root.Main", new TextField("Logo2Url", "Optionally enter URL to link to from 2nd logo image"));
     }
 }
