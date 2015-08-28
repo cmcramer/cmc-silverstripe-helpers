35334c7a810da1407f6ecb895c94d37c1b7952d9
Cathy Cramer <cmc@orionhost.net>
2015-08-28 17:38:02 -0600

    Add SiteTwitterFeed

---
+++ code/dataextensions/CmcSiteTwitterFeed.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * CmcSiteTwitterFeed
+ *
+ * @author cmc
+ * 
+ * @see - Twitter Embedded Timeline Documentation https://dev.twitter.com/web/embedded-timelines
+ *
+ * @usage -
+ *  First create widget in your Twitter account
+ *  
+ *  Fields for this DataObject correspond to these parts of the code from the Twitter Widget
+ *  
+ *  <a class="twitter-timeline" data-dnt="true" href="<TwitterTimelineUrl>"  
+ *      data-widget-id="<TwitterWidgetId>">Tweets <TwitterFeedName></a>
+ *      <script>....</script>
+ *      
+ *      Include "by/from" in TwitterFeedName field!!
+ *  
+ *      In _config/config.yml
+ *
+ *          SiteConfig:
+ *              extensions:
+ *                  - CmcSiteTwitterFeed
+ *
+ *      In template
+ *
+ *          $CmcTwitterFeed
+ *
+ */
+class CmcSiteSocialUrls extends DataExtension {
+    public static $db = array(
+        'TwitterTimelineUrl'    => 'Varchar(250)',
+        'TwitterWidgetId'       => 'Varchar(250)',
+        'TwitterFeedName'       => 'Varchar(250)',
+    );
+
+    public function updateCMSFields(FieldList $fields) {
+        $fields->addFieldToTab("Root.Main", new TextField("TwitterTimelineUrl", "Enter the full URL Twitter Timeline for this feed Widget"));
+        $fields->addFieldToTab("Root.Main", new TextField("TwitterWidgetId", "Enter the Twitter Widget ID"));
+        
+        $twitterFeedNameField = TextField::create("TwitterFeedName", "Enter Twitter Feed name (including by/from)")
+            ->setDescription('Fields for this DataObject correspond to these parts of the code from the Twitter Widget<br>
+                <code>&lt;a class="twitter-timeline" data-dnt="true" href="&laquo;TwitterTimelineUrl&raquo;"  
+                data-widget-id="&laquo;TwitterWidgetId&raquot;"&gt;Tweets &laquo;TwitterFeedName&raquot;&lt;&#47;a&gt;
+                &lt;script&gt;....$lt;&#47;script&gt;
+                <br><br>Include "by&#47;from" in TwitterFeedName field!!');
+        
+        $fields->addFieldToTab("Root.Main", $twitterFeedNameField);
+    }
+    
+    public function CmcTwitterFeed() {
+        $twitterFeed = false;
+        if ($this->owner->TwitterTimelineUrl && $this->TwitterTimelineUrl != '' &&
+            $this->TwitterWidgetId && $this->TwitterWidgetId != '' && 
+            $this->TwitterFeedName && $this->TwitterFeedName != '') {
+
+                $twitterFeed = <<<EOT
+                <a class="twitter-timeline" data-dnt="true" href="{$this->TwitterTimelineUrl}"  data-widget-id="{$this->TwitterWidgetId}" data-chrome="noheader">Tweets {$this->TwitterFeedName}</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+EOT;
+        }
+        
+        return $twitterFeed;
+    }
+}
