d9edbc5786574353e81f5d4c7c6b00236ac3d18c
Cathy Cramer <cmc@orionhost.net>
2015-08-28 11:56:43 -0600

    Add Title text for URLs

--- code/dataextensions/CmcSiteLogo.php
+++ code/dataextensions/CmcSiteLogo.php
@@ -23,14 +23,18 @@ class CmcSiteLogo extends DataExtension {
     );
     
     public static $db = array(
-        'LogoUrl'   => 'Varchar(250)',
-        'Logo2Url'   => 'Varchar(250)',
+        'LogoUrl'       => 'Varchar(250)',
+        'LogoUrlTitle'  => 'Varchar(120)',
+        'Logo2Url'      => 'Varchar(250)',
+        'Logo2UrlTitle' => 'Varchar(120)',
     );
     
     public function updateCMSFields(FieldList $fields) {
         $fields->addFieldsToTab('Root.Main', new UploadField('LogoImage', 'Choose an image for your site logo'));
         $fields->addFieldToTab("Root.Main", new TextField("LogoUrl", "Optionally enter URL to link to from logo image"));
+        $fields->addFieldToTab("Root.Main", new TextField("LogoUrlTitle", "Title/Tooltip text for URL above"));
         $fields->addFieldsToTab('Root.Main', new UploadField('Logo2Image', 'Choose an image for your 2nd site logo'));
         $fields->addFieldToTab("Root.Main", new TextField("Logo2Url", "Optionally enter URL to link to from 2nd logo image"));
+        $fields->addFieldToTab("Root.Main", new TextField("Logo2UrlTitle", "Title/Tooltip text for URL above"));
     }
 }
