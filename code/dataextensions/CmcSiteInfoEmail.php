b290b3f76cd9748462ae83625241dfb69a0d6c06
Cathy Cramer <cmc@orionhost.net>
2016-03-28 21:03:01 -0600

    Fixes

--- code/dataextensions/CmcSiteInfoEmail.php
+++ code/dataextensions/CmcSiteInfoEmail.php
@@ -26,6 +26,7 @@ class CmcSiteInfoEmail extends DataExtension {
 
     public function updateCMSFields(FieldList $fields) {
         $fields->addFieldToTab("Root.Main", new TextField("Email", "Site Info Email"));
+        $fields->addFieldToTab("Root.Main", new TextField("Subject", "Email Subject"));
     }
     
     public function EncodedSiteInfoEmail($label='') {
@@ -33,13 +34,21 @@ class CmcSiteInfoEmail extends DataExtension {
             $label=$this->owner->Email;
         }
         if ($this->owner->Subject != '') { //set subject
-            $mailString  = '<a href="' . encode('mailto:' . $this->owner->Email);
+            $mailString  = '<a href="' . $this->_encode('mailto:' . $this->owner->Email);
             $mailString .= '?subject=' . $this->owner->Subject . '">';
         } else {              //no subject
-            $mailString  = '<a href="' . encode('mailto:' . $this->owner->Email) . '" >';
+            $mailString  = '<a href="' . $this->_encode('mailto:' . $this->owner->Email) . '" >';
         }
-        $mailString  .= '<span class="email">' . encode($label) . '</span></a>';
+        $mailString  .= '<span class="email">' . $this->_encode($label) . '</span></a>';
         return $mailString;
+    }
+    
+    private function _encode($string)  {
+        $encoded = '';
+        for ($i=0; $i < strlen($string); $i++) {
+        $encoded .= '&#'.ord(substr($string,$i)).';';
+        }
+        return $encoded;
     }
     
 }
