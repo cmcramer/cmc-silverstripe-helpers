63e49ab690c4596e5789d5ed1d06244c2aad1fcb
Cathy Cramer <cmc@orionhost.net>
2016-05-06 13:31:03 -0600

    Add Thumbnail Size to List with Image Items

--- code/dataextensions/CmcItemWithImageList.php
+++ code/dataextensions/CmcItemWithImageList.php
@@ -23,6 +23,8 @@ class CmcItemWithImageList extends CmcItemList {
 	    'ListNotes'    => 'HTMLText',
 	    'ExpandCollapseLabel'  => 'Text',
 	    'ListItemName' => 'Text',
+        'ThumbnailWidth'    => 'Int',
+        'ThumbnailHeight'   => 'Int',
 	);
 
 	
@@ -30,7 +32,47 @@ class CmcItemWithImageList extends CmcItemList {
 	         'ListItems'    => 'CmcListItemWithImage',
 	);
 	
+	private static $defaults = array(
+	       'ThumbnailWidth'    => 120,
+	       'ThumbnailHeight'   => 120,
+	);
+	
+	
+	public function updateCMSFields(FieldList $fields) {
+	    
+	    self::populateDefaults();
+        // Create a default configuration for the new GridField, allowing record editing
+        $listGridConfig = GridFieldConfig_RelationEditor::create();
+        $listGridConfig->addComponent(new GridFieldSortableRows('ItemOrder'));
+        $listGridConfig->getComponentByType('GridFieldAddNewButton')->setButtonName("Add {$this->_getAddButtonLabel()}");
+        // Create a gridfield to hold the faqs relationship
+        $listItemsField = new GridField(
+        		'ListItems', // Field name
+        		$this->_tabName(), // Field title
+        		$this->owner->ListItems(), // List of all related news faqs
+        		$listGridConfig
+        );
+        //$listItemsField->getConfig()
+        
+		// Create a tab named "List" and add our field to it
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new TextField("ListTitle", "List Title"));
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new TextField("ListItemName", "List Item Name"));
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new TextField("ExpandCollapseLabel", "Expand/Collapse Label (optional)"));
+		$fields->addFieldToTab("Root.{$this->_tabName()}", $listItemsField);
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new HtmlEditorField("ListNotes", "List Notes"));
+        
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new NumericField('ThumbnailWidth', 'Thumbnail Width'), 'ListItems');
+        $fields->addFieldToTab("Root.{$this->_tabName()}", new NumericField('ThumbnailHeight', 'Thumbnail Height'), 'ListItems');
+        return $fields;
+	}
+	
 
 	
+	public function onBeforeWrite() {
+	    if (! $this->owner->ID) {
+	        $this->owner->ThumbnailWidth = self::$defaults['ThumbnailWidth'];
+	        $this->owner->ThumbnailHeight = self::$defaults['ThumbnailHeight'];
+	    }
+	}
 	
 }
--- code/model/CmcListItemWIthImage.php
+++ code/model/CmcListItemWIthImage.php
@@ -4,7 +4,9 @@ class CmcListItemWithImage extends CmcListItem {
     private static $singular_name = 'List Item with Image';
     private static $plural_name = 'List Items with Image';
     
-        
+    private static $db = array(
+    );
+    
     private static $has_one = array(
         'Image'     => 'Image',
     );
@@ -15,6 +17,14 @@ class CmcListItemWithImage extends CmcListItem {
         'ItemContentChopped'    => 'ItemContent',
         'Hide.NiceCMS'          => 'Hidden',  
     );
-    
+
+//     public function getCMSFields() {
+//         $fields = parent::getCMSFields();
+//         $fields->removeByName('ThumbnailWidth');
+//         $fields->removeByName('ThumbnailHeight');
+//         $fields->addFieldToTab('Root.Main', new NumericField('ThumbnailWidth', 'Thumbnail Width'), 'ItemTitle');
+//         $fields->addFieldToTab('Root.Main', new NumericField('ThumbnailHeight', 'Thumbnail Height'), 'ItemTitle');
+//         return $fields;
+//     }
 
 }
