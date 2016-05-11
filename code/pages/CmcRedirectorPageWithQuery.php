ff88f75fa3f5ea4bbf86148a5f563fc74e2afc65
Cathy Cramer <cmc@orionhost.net>
2016-05-11 12:52:18 -0600

    Added CmcRedirectorPageWithQuery
    Page class that extends RedirectorPage to allow for links to internal
    pages with query string or routing paramaters.

---
+++ code/pages/CmcRedirectorPageWithQuery.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * 
+ * @author cmc
+ * 
+ * Adds field for parameters or query string to internal page link
+ *
+ * Add extension to RedirectorPage in mysite/_config/config.yml
+ * 
+ */
+class CmcRedirectorPageWithQuery extends RedirectorPage {
+
+
+	private static $singular_name = 'Redirector Page with Route or Query';
+	private static $plural_name = 'Redirector Pages with Route or Query';
+	
+	private static $description = 'Redirects to a different page. Option to add query or route to internal page.';
+	
+
+	private static $db = array(
+	    'QueryString'          => 'Text',
+	);
+	
+	
+	public function getCMSFields() {
+	    $fields = parent::getCMSFields();
+	    $fields->removeByName('LinkToID');
+	    $fields->removeByName('ExternalURL');
+	    
+	    $internalPageField = new DisplayLogicWrapper(new TreeDropdownField(
+	                               'LinkToID', 'Page on this website', 'SiteTree'));
+	    $internalPageField->displayIf("RedirectionType")->isEqualTo("Internal");
+	    
+	    
+	    $fields->addFieldToTab('Root.Main', $internalPageField);
+	    $fields->addFieldsToTab('Root.Main', 
+	           $queryField = new TextField('QueryString', 'Route or Query String'));
+	    
+	    $queryField->displayIf("RedirectionType")->isEqualTo("Internal");
+	    
+
+	    $fields->addFieldsToTab('Root.Main',
+	        $externalUrlField = new TextField('ExternalURL', 'Other website URL'));
+	     
+	    $externalUrlField->displayIf("RedirectionType")->isEqualTo("External");
+	    
+	    
+	    return $fields;
+	    
+	}
+	
+	
+	/**
+	 * Overide Link function
+	 */
+    public function redirectionLink() {
+        $link = parent::redirectionLink();
+        if($this->RedirectionType == 'Internal' && $this->QueryString && $this->QueryString != '') {
+            return $link.$this->QueryString;
+        }
+        return $link;
+    }
+	
+    
+	
+}
+
+
+class CmcRedirectorPageWithQuery_Controller extends RedirectorPage_Controller {
+    
+}
