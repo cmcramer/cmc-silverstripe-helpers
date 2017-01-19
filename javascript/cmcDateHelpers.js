20c7481c2a0d43f6d9ec87fc3dfe04620c7cb4a0
Cathy Cramer <cmc@orionhost.net>
2017-01-19 15:29:36 -0700

    Error catching
    Protect against null value passed to date functions

--- javascript/cmcDateHelpers.js
+++ javascript/cmcDateHelpers.js
@@ -33,21 +33,23 @@ var cmcArrMonthNames = [
 //date string format YYYY-MM-DD to 8 Dec
 //http://www.w3schools.com/jsref/jsref_obj_date.asp
 function cmcDDMonFormat(strDate) {
-	var objDate = new Date(strDate.split(' ')[0]);
-	objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
-	return objDate.getDate() + " " + cmcArrShortMonthNames[objDate.getMonth()];
-    //var dateArr = strDate.split('-');
-	//return dateString + " HI!";
+	if (strDate != null) {
+		var objDate = new Date(strDate.split(' ')[0]);
+		objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
+		return objDate.getDate() + " " + cmcArrShortMonthNames[objDate.getMonth()];
+	}
+	return '';
 }
 
 //date string format YYYY-MM-DD to 8 Dec 2016
 //http://www.w3schools.com/jsref/jsref_obj_date.asp
 function cmcDDMonYYYYFormat(strDate) {
-	var objDate = new Date(strDate.split(' ')[0]);
-	objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
-	 return objDate.getDate() + " " + cmcArrShortMonthNames[objDate.getMonth()]  + " " + objDate.getFullYear();
-  //var dateArr = strDate.split('-');
-	//return dateString + " HI!";
+	if (strDate != null) {
+		var objDate = new Date(strDate.split(' ')[0]);
+		objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
+		 return objDate.getDate() + " " + cmcArrShortMonthNames[objDate.getMonth()]  + " " + objDate.getFullYear();
+	}
+	return '';
 }
 
 
@@ -56,19 +58,21 @@ function cmcDDMonYYYYFormat(strDate) {
 //date string format YYYY-MM-DD to 8 December
 //http://www.w3schools.com/jsref/jsref_obj_date.asp
 function cmcDDMonthFormat(strDate) {
-	var objDate = new Date(strDate.split(' ')[0]);
-	objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
-	 return objDate.getDate() + " " + cmcArrMonthNames[objDate.getMonth()];
-  //var dateArr = strDate.split('-');
-	//return dateString + " HI!";
+	if (strDate != null) {
+		var objDate = new Date(strDate.split(' ')[0]);
+		objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
+		 return objDate.getDate() + " " + cmcArrMonthNames[objDate.getMonth()];
+	}
+	return '';
 }
 
 //date string format YYYY-MM-DD to 8 December
 //http://www.w3schools.com/jsref/jsref_obj_date.asp
 function cmcDDMonthYYYYFormat(strDate) {
-	var objDate = new Date(strDate.split(' ')[0]);
-	objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
-	 return objDate.getDate() + " " + cmcArrMonthNames[objDate.getMonth()]  + " " + objDate.getFullYear();
-//var dateArr = strDate.split('-');
-	//return dateString + " HI!";
+	if (strDate != null) {
+		var objDate = new Date(strDate.split(' ')[0]);
+		objDate.setTime(objDate.getTime() + objDate.getTimezoneOffset()*60*1000);
+		 return objDate.getDate() + " " + cmcArrMonthNames[objDate.getMonth()]  + " " + objDate.getFullYear();
+	}
+	return '';
 }
